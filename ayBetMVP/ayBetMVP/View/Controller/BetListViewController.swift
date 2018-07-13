65974a87b3ce0f7d93e298852ef1e02b29bc3e9e
churtadom <carlos.andres.hurtado.montenegro@everis.com>
2018-07-13 11:59:52 -0500

    Added basic UI

--- ayBetMVP/ayBetMVP/View/Controller/BetListViewController.swift
+++ ayBetMVP/ayBetMVP/View/Controller/BetListViewController.swift
@@ -19,20 +19,43 @@ class BetListViewController: UIViewController {
 
 }
 
-//extension BetListViewController: UITableViewDelegate {
-//    
-//}
-//
-//extension BetListViewController: UITableViewDataSource {
-//    
-//}
+extension BetListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let nib = UINib(nibName: "MatchHeaderView", bundle: nil)
+        guard let view = nib.instantiate(withOwner: self, options: nil).first as? MatchHeaderView else { return nil }
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 100
+    }
+}
+
+extension BetListViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "betCell", for: indexPath)
+    }
+    
+}
 
 extension BetListViewController {
     
     fileprivate func initialSetup() {
-        title = "ayBet"
-//        betListTableView.dataSource = self
-//        betListTableView.delegate = self
+        betListTableView.dataSource = self
+        betListTableView.delegate = self
     }
     
 }
